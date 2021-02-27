3e43a79509c9e3c91ebf97473e43a7d66025bce5
VirReal <cangokceaslan@outlook.com>
2021-02-27 18:37:06 +0300

    Naming Changed and Day_10 Added

---
+++ Day_10/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        string binary = Convert.ToString(n, 2);
+
+        string[] repeat = binary.Split('0');
+
+        string longest = "";
+
+        for(int i = 0; i < repeat.Length; i++)
+        {
+            if (repeat[i].Length > longest.Length)
+            {
+                longest = repeat[i];
+            }
+        }
+
+        Console.WriteLine(longest.Length);
+
+        Console.ReadLine();
+    }
+}
