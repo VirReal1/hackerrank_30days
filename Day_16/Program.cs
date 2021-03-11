c1e4cd9235724af1efbb45a17df06c4a057c6de5
VirReal <cangokceaslan@outlook.com>
2021-03-12 00:56:36 +0300

    Day_16 Added

---
+++ Day_16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        try
+        {
+            string S = Console.ReadLine();
+
+            Console.WriteLine(Convert.ToInt32(S));
+        }
+        catch
+        {
+            Console.WriteLine("Bad String");
+        }
+        Console.ReadLine();
+    }
+}
